f656d062681684727587462eee8bd3f34f04dbf4
Jeffrey McNally-Dawes <jeffmcnallydawes@gmail.com>
2017-02-26 17:21:04 -0800

    listen to users added

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var firebase = require('firebase');
+
 var app = express();
 
 var config = {
@@ -10,6 +12,10 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+firebase.database().ref('users').on('child_added', function(snap) {
+  console.log(snap.key);
+});
 
 app.listen(3000, function () {
   console.log('Running Clik on port 3000!')
